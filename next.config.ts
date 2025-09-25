b03e45da7c19e604c1bf29d16a5d2e49e9e6dbd2
Leanitech <akarmakar846@gmail.com>
2025-09-25 21:45:00 +0530

    feat(i18n): add next-intl plugin for internationalization support
    Integrate next-intl plugin to enable internationalization in the Next.js application. The plugin is configured with the i18n request handler located at './i18n/request.ts'.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const isDev = process.env.NODE_ENV === 'development' ? false : true;
 
@@ -95,4 +96,8 @@ const nextConfig: NextConfig = {
   ],
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
+export default withNextIntl(nextConfig);
