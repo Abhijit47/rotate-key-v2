079fe0f0a26d37209ead7033681fc2e0b28ec4b7
Leanitech <akarmakar846@gmail.com>
2025-09-26 08:49:51 +0530

    refactor(config): replace config import with env variables for Cloudinary and remove unused config file

--- configs/cloudinary.ts
+++ configs/cloudinary.ts
@@ -1,11 +1,11 @@
+import { env } from '@/env';
 import { v2 as cloudinary } from 'cloudinary';
-import config from './index';
 
 // Configuration
 cloudinary.config({
-  cloud_name: config.cloudinary.cloudName, // Click 'View API Keys' above to copy your cloud name
-  api_key: config.cloudinary.apiKey, // Click 'View API Keys' above to copy your API key
-  api_secret: config.cloudinary.apiSecret, // Click 'View API Keys' above to copy your API secret
+  cloud_name: env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
