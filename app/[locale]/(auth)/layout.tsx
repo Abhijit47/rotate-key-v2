3eb1e514a9d492c23247cae2ae0095bf16a46090
Leanitech <akarmakar846@gmail.com>
2025-09-26 12:32:43 +0530

    refactor(auth): remove async from AuthLayout function for improved performance

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     "Unlock the Door to Your Next Adventure with Rotate Keys. Rotate Keys is not just a platform; it's a community of like-minded individuals sharing the joy of exploration and discovery. Your dream house swap is just a click away.",
 };
 
-export default async function AuthLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
