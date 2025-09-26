313b6cde53a31c4346a488cf21ae03abb055c51b
Leanitech <akarmakar846@gmail.com>
2025-09-26 08:48:25 +0530

    feat(env): add new environment variables for Neon, Cloudinary, Novu, and Resend APIs

--- env.ts
+++ env.ts
@@ -4,6 +4,9 @@ import { z } from 'zod/v4';
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
+    NEON_API_KEY: z.string().min(1),
+    CLOUDINARY_API_KEY: z.string().min(1),
+    CLOUDINARY_API_SECRET: z.string().min(1),
     CLOUDINARY_UPLOAD_PRESET_NAME: z.string().min(1),
     CLOUDINARY_BASE_FOLDER_NAME: z.string().min(1),
     CLOUDINARY_SIGNING_MODE: z.string().superRefine((val, ctx) => {
@@ -14,28 +17,30 @@ export const env = createEnv({
         });
       }
     }),
-    CLOUDINARY_API_KEY: z.string().min(1),
-    CLOUDINARY_API_SECRET: z.string().min(1),
-    NEON_API_KEY: z.string().min(1),
     STREAM_API_SECRET: z.string().min(1),
+    NOVU_SECRET_KEY: z.string().min(1),
+    NOVU_API_URL: z.string().min(1),
+    NOVU_API_EU_URL: z.string().min(1),
+    RESEND_API_KEY_FULL_ACCESS: z.string().min(1),
+    RESEND_API_KEY_SENDING_ACCESS: z.string().min(1),
   },
   client: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
     NEXT_PUBLIC_C15T_URL: z.url(),
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string().min(1),
     NEXT_PUBLIC_DEV_BASE_URL: z.url(),
     NEXT_PUBLIC_PROD_BASE_URL: z.url(),
     NEXT_PUBLIC_STREAM_API_KEY: z.string().min(1),
     NEXT_PUBLIC_DATABUDDY_CLIENT_ID: z.string().min(1),
+    NEXT_PUBLIC_NOVU_APP_ID_DEV: z.string().min(1),
+    NEXT_PUBLIC_NOVU_APP_ID_PROD: z.string().min(1),
   },
+
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   // runtimeEnv: {
-  //   DATABASE_URL: process.env.DATABASE_URL,
-  //   OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
   // },
+
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
     NEXT_PUBLIC_C15T_URL: process.env.NEXT_PUBLIC_C15T_URL,
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
       process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -44,5 +49,7 @@ export const env = createEnv({
     NEXT_PUBLIC_STREAM_API_KEY: process.env.NEXT_PUBLIC_STREAM_API_KEY,
     NEXT_PUBLIC_DATABUDDY_CLIENT_ID:
       process.env.NEXT_PUBLIC_DATABUDDY_CLIENT_ID,
+    NEXT_PUBLIC_NOVU_APP_ID_DEV: process.env.NEXT_PUBLIC_NOVU_APP_ID_DEV,
+    NEXT_PUBLIC_NOVU_APP_ID_PROD: process.env.NEXT_PUBLIC_NOVU_APP_ID_PROD,
   },
 });
